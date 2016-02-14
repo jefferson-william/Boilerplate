f485714484c99dfd6340c743d82168dfe7df86aa
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2016-02-14 17:23:38 -0200

    Carregar scripts por dependência através do RequireJS

--- WebApplication1/Scripts/Bootstrap.js
+++ WebApplication1/Scripts/Bootstrap.js
@@ -3,17 +3,48 @@
 require.config({
     baseUrl: '/Scripts',
     paths: {
+		// Arquivos gerais e/ou de configurações
+		'Main': 'Main',
+
+    	// AngularJS e Material
 		'Angular': '../Content/Components/angular/angular',
 		'AngularAnimate': '../Content/Components/angular-animate/angular-animate',
 		'AngularAria': '../Content/Components/angular-aria/angular-aria',
 		'AngularMaterial': '../Content/Components/angular-material/angular-material',
 		'AngularMessages': '../Content/Components/angular-messages/angular-messages',
-		'Main': 'Main'
+
+		// Plugins do RequireJS que permite carregar diversos tipos de arquivos
+        'async': '../Content/Components/require-plugins/src/async',
+        'font': '../Content/Components/require-plugins/src/font',
+        'image': '../Content/Components/require-plugins/src/image',
+        'json': '../Content/Components/require-plugins/src/json'
+    },
+    shim: {
+        'AngularAnimate': {
+            deps: ['Angular']
+        },
+        'AngularAria': {
+            deps: ['Angular']
+        },
+        'AngularMessages': {
+            deps: ['Angular']
+        },
+        'AngularMaterial': {
+            deps: ['Angular']
+        },
+        'Main': {
+            deps: ['Angular', 'AngularAnimate', 'AngularAria', 'AngularMessages', 'AngularMaterial']
+        }
     }
 });
 
 require([
+    'Angular',
+    'AngularAnimate',
+    'AngularAria',
+    'AngularMessages',
+    'AngularMaterial',
     'Main'
-], function (Main) {
+], function (Angular, AngularAnimate, AngularAria, AngularMessages, AngularMaterial, Main) {
     console.log('Hello!');
 });
